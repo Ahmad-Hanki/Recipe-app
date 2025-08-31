ed813eacd4ef464394a66edce85b7b65095eeba1
Ahmad <itxti909@gmail.com>
2025-08-31 23:13:13 +0300

    feat: jobs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,14 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import router from "./apis/favorites.js";
+import job from "./config/cron.js";
+
 const app = express();
 const PORT = ENV.PORT;
+
+if (ENV.NODE_ENV == "production") job.start();
 app.use(express.json());
 app.use("/api", router);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-
-
